6e36e51a1cf1b7907aa6ae8a4a31470462f0df6e
Ivan Kozik <ivan@ludios.org>
2023-04-09 10:16:35 +0000

    Revert "No longer causes rust-analyzer problem"
    This reverts commit c6d97b03fb19e415f2dd03c191c9d5cd2293b153.

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,14 @@ use futures_async_stream::stream;
 
 type Aes128Ctr = ctr::Ctr64BE<aes::Aes128>;
 
-fn empty_stream() -> impl Stream<Item = ()> {
+async fn empty_stream() -> impl Stream<Item = ()> {
     futures::stream::empty()
 }
 
 fn stream_file() -> impl Stream<Item = ()> {
     #[stream]
     async move {
-        let stream = empty_stream();
+        let stream = empty_stream().await;
         let key = GenericArray::from_slice(&[0; 16]);
         let cipher = Aes128Ctr::new(key, key);
 
